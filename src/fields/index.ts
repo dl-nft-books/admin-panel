23fb94b4e13b9784bb2c907bde2a0eda65368829
Oleh Tryfonov <oleg.himik.trifon@gmail.com>
2022-10-27 22:31:03 +0300

    added file field

--- src/fields/index.ts
+++ src/fields/index.ts
@@ -1,4 +1,5 @@
 export { default as InputField } from '@/fields/InputField.vue'
 export { default as CheckboxField } from '@/fields/CheckboxField.vue'
+export { default as FileField } from '@/fields/FileField.vue'
 
 /* TODO: Create Select, Textarea */

8194ce4863437c700ebd5fbf53bfbeddcc37d5b2
electr1xx <aleksandrplotnikov1997@gmail.com>
2022-11-30 15:40:24 +0200

    added pages

--- src/forms/NftForm.vue
+++ src/forms/NftForm.vue
@@ -1,9 +1,108 @@
+<template>
+  <form class="nft-form" @submit.prevent="submit">
+    <h3 class="nft-form__subtitle">
+      {{ $t('nft-form.uploading-subtitle') }}
+    </h3>
+    <div class="nft-form__uploadings">
+      <file-field
+        class="nft-form__upload-field"
+        v-model="form.photo"
+        :file-extensions="['jpg', 'png', 'jpeg']"
+        :label="$t('nft-form.file-field-image-title')"
+        :note="$t('nft-form.file-field-image-description')"
+        :error-message="getFieldErrorMessage('photo')"
+        :disabled="isFormDisabled"
+      />
+      <file-field
+        class="nft-form__upload-field"
+        v-model="form.book"
+        :file-extensions="['pdf']"
+        :label="$t('nft-form.file-field-pdf-title')"
+        :note="$t('nft-form.file-field-pdf-description')"
+        :error-message="getFieldErrorMessage('book')"
+        :disabled="isFormDisabled"
+        :max-size="MAX_BOOK_SIZE"
+      />
+    </div>
+    <h3 class="nft-form__subtitle">
+      {{ $t('nft-form.details-subtitle') }}
+    </h3>
+    <div class="nft-form__details">
+      <input-field
+        v-model="form.name"
+        :max-length="FIELD_LENGTH.name"
+        :placeholder="$t('nft-form.input-name-placeholder')"
+        :label="$t('nft-form.input-name-label')"
+        :error-message="getFieldErrorMessage('name')"
+        :disabled="isFormDisabled"
+        @blur="touchField('name')"
+      />
+      <amount-field
+        v-model="form.price"
+        :placeholder="$t('nft-form.input-price-placeholder')"
+        :label="$t('nft-form.input-price-label')"
+        :error-message="getFieldErrorMessage('price')"
+        :disabled="isFormDisabled"
+        @blur="touchField('price')"
+      />
+      <input-field
+        v-model="form.symbol"
+        :max-length="FIELD_LENGTH.tokenSymbol"
+        :placeholder="$t('nft-form.input-symbol-placeholder')"
+        :label="$t('nft-form.input-symbol-label')"
+        :error-message="getFieldErrorMessage('symbol')"
+        :disabled="isFormDisabled"
+        @blur="touchField('symbol')"
+      />
+      <textarea-field
+        class="nft-form__textarea"
+        v-model="form.description"
+        :max-length="FIELD_LENGTH.description"
+        :placeholder="$t('nft-form.textarea-description-placeholder')"
+        :label="$t('nft-form.textarea-description-label')"
+        :error-message="getFieldErrorMessage('description')"
+        :disabled="isFormDisabled"
+        @blur="touchField('description')"
+      />
+      <div class="nft-form__action-buttons">
+        <app-button
+          class="nft-form__button"
+          scheme="flat"
+          size="small"
+          :text="$t('nft-form.cancel-button')"
+          :route="{ name: $routes.nfts }"
+          :disabled="isFormDisabled"
+        />
+
+        <template v-if="isValidChain">
+          <app-button
+            type="submit"
+            size="small"
+            class="nft-form__button"
+            :text="$t('nft-form.create-button')"
+            :disabled="isFormDisabled"
+          />
+        </template>
+        <template v-else>
+          <app-button
+            type="button"
+            class="nft-form__button"
+            size="small"
+            :text="$t('nft-form.switch-chain-button')"
+            @click="provider.switchChain(config.CHAIN_ID)"
+          />
+        </template>
+      </div>
+    </div>
+  </form>
+</template>
+
 <script lang="ts" setup>
 import { InputField, TextareaField, AmountField, FileField } from '@/fields'
 import { computed, reactive } from 'vue'
 import { AppButton } from '@/common'
 import { FIELD_LENGTH, ROUTE_NAMES } from '@/enums'
-import { useRoute, useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 import { useTokenFactory, useForm, useFormValidation } from '@/composables'
 import { useWeb3ProvidersStore } from '@/store'
 import { storeToRefs } from 'pinia'
@@ -18,17 +117,12 @@ const MIN_PRICE_VALUE = '0.01'
 const MAX_BOOK_SIZE = 500 // mb
 
 const { t } = useI18n()
-const route = useRoute()
 const router = useRouter()
 const { provider } = storeToRefs(useWeb3ProvidersStore())
 
 const tokenFactory = useTokenFactory(
   provider.value,
   config.TOKEN_FACTORY_CONTRACT_ADDRESS,
-)
-
-const pageTitle = computed(() =>
-  ROUTE_NAMES.nftItemEdit === route.name ? 'edit' : 'create',
 )
 
 const isValidChain = computed(
@@ -114,121 +208,7 @@ const submit = async () => {
 }
 </script>
 
-<template>
-  <div class="nft-form">
-    <h2 class="nft-form__title">
-      {{ $t(`nft-form.${pageTitle}-title`) }}
-    </h2>
-    <form @submit.prevent="submit">
-      <h3 class="nft-form__subtitle">
-        {{ $t('nft-form.uploading-subtitle') }}
-      </h3>
-      <div class="nft-form__uploadings">
-        <file-field
-          class="nft-form__upload-field"
-          v-model="form.photo"
-          :file-extensions="['jpg', 'png', 'jpeg']"
-          :label="$t('nft-form.file-field-image-title')"
-          :note="$t('nft-form.file-field-image-description')"
-          :error-message="getFieldErrorMessage('photo')"
-          :disabled="isFormDisabled"
-        />
-        <file-field
-          class="nft-form__upload-field"
-          v-model="form.book"
-          :file-extensions="['pdf']"
-          :label="$t('nft-form.file-field-pdf-title')"
-          :note="$t('nft-form.file-field-pdf-description')"
-          :error-message="getFieldErrorMessage('book')"
-          :disabled="isFormDisabled"
-          :max-size="MAX_BOOK_SIZE"
-        />
-      </div>
-      <h3 class="nft-form__subtitle">
-        {{ $t('nft-form.details-subtitle') }}
-      </h3>
-      <div class="nft-form__details">
-        <input-field
-          v-model="form.name"
-          :max-length="FIELD_LENGTH.name"
-          :placeholder="$t('nft-form.input-name-placeholder')"
-          :label="$t('nft-form.input-name-label')"
-          :error-message="getFieldErrorMessage('name')"
-          :disabled="isFormDisabled"
-          @blur="touchField('name')"
-        />
-        <amount-field
-          v-model="form.price"
-          :placeholder="$t('nft-form.input-price-placeholder')"
-          :label="$t('nft-form.input-price-label')"
-          :error-message="getFieldErrorMessage('price')"
-          :disabled="isFormDisabled"
-          @blur="touchField('price')"
-        />
-        <input-field
-          v-model="form.symbol"
-          :max-length="FIELD_LENGTH.tokenSymbol"
-          :placeholder="$t('nft-form.input-symbol-placeholder')"
-          :label="$t('nft-form.input-symbol-label')"
-          :error-message="getFieldErrorMessage('symbol')"
-          :disabled="isFormDisabled"
-          @blur="touchField('symbol')"
-        />
-        <textarea-field
-          class="nft-form__textarea"
-          v-model="form.description"
-          :max-length="FIELD_LENGTH.description"
-          :placeholder="$t('nft-form.textarea-description-placeholder')"
-          :label="$t('nft-form.textarea-description-label')"
-          :error-message="getFieldErrorMessage('description')"
-          :disabled="isFormDisabled"
-          @blur="touchField('description')"
-        />
-        <div class="nft-form__action-buttons">
-          <app-button
-            class="nft-form__button"
-            scheme="flat"
-            size="small"
-            :text="$t('nft-form.cancel-button')"
-            :route="{ name: $routes.nfts }"
-            :disabled="isFormDisabled"
-          />
-
-          <template v-if="isValidChain">
-            <app-button
-              type="submit"
-              size="small"
-              class="nft-form__button"
-              :text="$t('nft-form.create-button')"
-              :disabled="isFormDisabled"
-            />
-          </template>
-          <template v-else>
-            <app-button
-              type="button"
-              class="nft-form__button"
-              size="small"
-              :text="$t('nft-form.switch-chain-button')"
-              @click="provider.switchChain(config.CHAIN_ID)"
-            />
-          </template>
-        </div>
-      </div>
-    </form>
-  </div>
-</template>
-
 <style lang="scss" scoped>
-.nft-form__title {
-  font-weight: 600;
-  font-size: toRem(40);
-  margin-bottom: toRem(30);
-
-  @include respond-to(small) {
-    font-size: toRem(30);
-  }
-}
-
 .nft-form__subtitle {
   font-size: toRem(24);
   font-weight: 600;
---
+++ src/pages/EditNftPage.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="edit-nft-page">
+    <h2 class="edit-nft-page__title">
+      {{ $t('edit-nft-page.title') }}
+    </h2>
+    <template v-if="isLoaded">
+      <template v-if="isLoadFailed">
+        <error-message :message="$t('edit-nft-page.error-msg')" />
+      </template>
+      <template v-else>
+        <nft-form />
+      </template>
+    </template>
+    <template v-else>
+      <loader />
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import NftForm from '@/forms/NftForm.vue'
+import { getBookById } from '@/api'
+import { ref } from 'vue'
+import { ErrorHandler } from '@/helpers'
+import { BookRecord } from '@/records'
+import { Loader, ErrorMessage } from '@/common'
+
+const props = defineProps<{
+  id: string
+}>()
+
+const book = ref<BookRecord>()
+const isLoaded = ref(false)
+const isLoadFailed = ref(false)
+
+const loadBook = async () => {
+  try {
+    const { data } = await getBookById(props.id)
+    book.value = new BookRecord(data)
+  } catch (e) {
+    isLoadFailed.value = true
+    ErrorHandler.processWithoutFeedback(e)
+  }
+  isLoaded.value = true
+}
+
+loadBook()
+</script>
+
+<style lang="scss" scoped>
+.edit-nft-page__title {
+  font-weight: 600;
+  font-size: toRem(40);
+  margin-bottom: toRem(30);
+
+  @include respond-to(small) {
+    font-size: toRem(30);
+  }
+}
+</style>
